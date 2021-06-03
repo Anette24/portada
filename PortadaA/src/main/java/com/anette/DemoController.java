4a3205e0d2cbd239e7cf77c34267120dc4d3ff99
Anette <Anette@ANETTE>
2021-06-03 15:14:18 -0500

    portada

---
+++ PortadaA/src/main/java/com/anette/DemoController.java
@@ -0,0 +1,13 @@
+package com.anette;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	@GetMapping("/")
+	public String Portada() {
+		return "portada";
+	}
+	
+}
---
+++ PortadaA/src/main/java/com/anette/PortadaAApplication.java
@@ -0,0 +1,13 @@
+package com.anette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortadaAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortadaAApplication.class, args);
+	}
+
+}
